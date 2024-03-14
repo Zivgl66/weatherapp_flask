c5b1b495c95f7c635d172d60d5f9a7b12b0e9924
Ziv <95753868+Zivgl66@users.noreply.github.com>
2024-03-14 13:58:31 +0200

    Update api_calls.py

--- modules/api_calls.py
+++ modules/api_calls.py
@@ -4,7 +4,7 @@ import os
 
 def get_weather(location):
     # with a give string of location, make an api call and return the data parsed to json of the weather info
-    res = requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={os.environ.get("API_KEY")}&unitGroup=metric')
+    res = requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={os.environ.get('API_KEY')}&unitGroup=metric')
     if res.status_code == 200:
         data = res.json()
         return data
